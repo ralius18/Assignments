1c72efa8be93266296df6bf9307f3df44d1a2e12
Brad Stone <brayds97@gmail.com>
2016-07-26 19:40:45 +1200

    Dice roll implementation

--- Assignment 1/src/control/Dice.java
+++ Assignment 1/src/control/Dice.java
@@ -1,5 +1,29 @@
 package control;
 
-public class Dice {
+import java.util.Random;
 
+public class Dice {
+	
+	private int number;
+	private int sides;
+	
+	public Dice(int number, int sides){
+		this.number = number;
+		this.sides = sides;
+	}
+
+	public int roll(){
+		int result = 0;
+		if (sides >= 3){
+			for (int i = 0; i < number; i++){
+				Random random = new Random();
+				int roll = random.nextInt();
+				result += (roll % sides)+1;
+			}
+		}
+		else {
+			System.out.println("Number of sides must be at least 3");
+		}
+		return result;
+	}
 }
